2eb46c8c3808c35d51b3a57b73ddf2dbd3df54c8
Dmitry Kalinin <x.shreddered.x@gmail.com>
2021-11-18 17:43:02 +0300

    refactored greedy solution

--- module3/greedy/main.cpp
+++ module3/greedy/main.cpp
@@ -1,15 +1,20 @@
+#include <deque>
 #include <iostream>
-#include <vector>
 #include <utility>
 
+// enum для обозначения шагов алгоритма
 enum Step {
     Inc,
     Dec,
     Double
 };
 
-std::vector<Step> steps(std::size_t n) {
-    std::vector<Step> res;
+// Функция, возвращающая шаги для заданной половины состояния.
+std::deque<Step> steps(std::size_t n) {
+    std::deque<Step> res;
+    // count trailing zeros
+    // Результат работы функции - пара, где первый элемент - это само значение,
+    // второй элемент - параметр, который сдвинули вправо на полученное число.
     auto ctz = [](std::size_t n) {
         std::size_t res = 0;
         while (n && !(n % 2)) {
@@ -18,31 +23,44 @@ std::vector<Step> steps(std::size_t n) {
         }
         return std::make_pair(n, res);
     };
+    // Ключевой цикл алгоритма.
+    // Идём с "обратной" стороны - от n к 1.
+    // Идея заключается в выборе максимально быстрого варианта как уменьшить число,
+    // комбинируя операции +1, -1 и div 2.
     while (n > 1 && n != 3) {
         if (n % 2) {
+            // Если двоичная запись числа заканчивается на 1, то смотрим,
+            // в каком случае больше нулей с конца (т.о. число уменьшится
+            // быстрее).
             const auto [branch1, ctz1] = ctz(n + 1);
             const auto [branch2, ctz2] = ctz(n - 1);
             if (ctz1 > ctz2) {
                 n = branch1;
-                res.push_back(Step::Dec);
+                res.push_front(Step::Dec);
             } else {
                 n = branch2;
-                res.push_back(Step::Inc);
+                res.push_front(Step::Inc);
             }
+            // После выбора команды inc/dec убираем нули с конца с помощью команды dbl.
             for (std::size_t count = std::max(ctz1, ctz2); count; --count)
-                res.push_back(Step::Double);
+                res.push_front(Step::Double);
         } else {
+            // Иначе просто убираем все нули с конца
+            // с помощью команды dbl.
             auto [n1, ctz1] = ctz(n);
             for (; ctz1; --ctz1)
-                res.push_back(Step::Double);
+                res.push_front(Step::Double);
             n = n1;
         }
     }
+    // Исключением является число 3. Для него лучшая последовательность действий
+    // следующая: inc, dbl, inc.
     if (n == 3) {
-        res.push_back(Step::Inc);
-        res.push_back(Step::Double);
+        res.push_front(Step::Inc);
+        res.push_front(Step::Double);
     }
-    res.push_back(Step::Inc);
+    // Сначала всегда забираем у богача миллион.
+    res.push_front(Step::Inc);
     return res;
 }
 
@@ -50,7 +68,7 @@ int main() {
     std::size_t n;
     std::cin >> n;
     auto res = steps(n);
-    for (auto it = res.rbegin(); it != res.rend(); ++it) {
+    for (auto it = res.begin(); it != res.end(); ++it) {
         switch (*it) {
             case Step::Inc:
                 std::cout << "inc\n";
