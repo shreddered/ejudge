6165b7da4559115821b8bca072522285d3c0e9f7
Dmitry Kalinin <x.shreddered.x@gmail.com>
2021-11-25 18:29:05 +0300

    new types in prime numbers generator

--- module3/bloom_filter/Main.hs
+++ module3/bloom_filter/Main.hs
@@ -3,23 +3,21 @@ module Main where
 import Control.Applicative
 
 import Data.Array.Unboxed (UArray, (//), (!), array, indices)
-import Data.Bits (shiftL)
+import Data.Bits (Bits, shiftL)
 import Data.Bool (bool)
 import Data.Word
 
-import Text.Read (readMaybe)
-
 -- list of prime numbers
-primes :: [Int]
+primes :: Integral a => [a]
 primes = sieve [2..]
 
 -- sieve of Eratosthenes
-sieve :: [Int] -> [Int]
+sieve :: Integral a => [a] -> [a]
 sieve (x:xs) = let multof x n = n `rem` x == 0
                 in x : (sieve (filter (not . multof x) xs))
 
 -- 31st Mersenne number
-mersenne31 :: Int
+mersenne31 :: (Num a, Bits a) => a
 mersenne31 = (1 `shiftL` 31) - 1
 
 -- Bloom filter
